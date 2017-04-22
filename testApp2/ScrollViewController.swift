902d471f05cf91a3e0d6ab6e8c6f2672d1f10cad
HaLamUs <itzlamz9x@gmail.com>
2017-04-22 14:59:56 +0700

    StackView Anim

---
+++ testApp2/ScrollViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ScrollViewController.swift
+//  testApp2
+//
+//  Created by Ha Lam on 4/22/17.
+//  Copyright © 2017 Gotadi. All rights reserved.
+//
+
+import UIKit
+
+class ScrollViewController: UIViewController {
+  
+  @IBOutlet weak var orangeView: UIView!
+  
+  @IBOutlet weak var blueView: UIView!
+  
+  var isShowBlue = true
+  
+  
+  @IBAction func touchUpButton(_ sender: UIButton) {
+    isShowBlue = !isShowBlue
+    UIView.animate(withDuration: 0.5) {
+      self.blueView.isHidden = !self.isShowBlue
+    }
+    
+  }
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
